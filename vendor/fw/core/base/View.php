f462abe0ecb0a57c5df215fcd1fa9c2653104876
Dmitriy Kogut <kogut.dmitriy@gmail.com>
2019-01-31 12:47:34 +0200

    new template integration

--- vendor/fw/core/base/View.php
+++ vendor/fw/core/base/View.php
@@ -132,4 +132,14 @@ class View
         self::$meta['keywords'] = $keywords;
     }
 
+    public function getPart($file)
+    {
+        $file = APP . "/views/$file.php";
+
+        if(file_exists($file)) {
+            require_once $file;
+        } else {
+            echo "File $file not found ...";
+        }
+    }
 }
